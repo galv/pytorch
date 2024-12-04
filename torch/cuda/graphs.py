ea3be08b7d9371f31d6f2eaef0f742ae87eafbd2
Daniel Galvez <dgalvez@nvidia.com>
2024-12-03 18:30:56 -0800

    Document thread_cuda_stream_capture_mode.

--- torch/cuda/graphs.py
+++ torch/cuda/graphs.py
@@ -536,6 +536,38 @@ def make_graphed_callables(
 
 @contextlib.contextmanager
 def thread_cuda_stream_capture_mode(new_mode):
+    r"""Context manager that changes the current thread's stream capture mode to `new_mode` upon __enter__ and resets the thread's stream capture mode to its original value upon __exit__.
+
+    The only documentation on a thread's stream capture mode is here: https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__STREAM.html#group__CUDART__STREAM_1g9d0535d93a214cbf126835257b16ba85
+
+    However, it is a little bit inadequate, so here is a more in-depth description.
+
+    Both CPU threads and capturing cuda streams have a capture mode. A
+    cuda stream's capture mode is set at cudaStreamBeginCapture() and
+    can never be changed. Meanwhile all CPU threads start with a capture
+    mode of cudaStreamCaptureModeGlobal, which can be changed at any
+    time.
+
+    Whenever a thread executes an unsafe CUDA action while CUDA
+    streams are capturing, it follows the following logic to determine
+    whether to invalidate those streams:
+
+    if capture_mode_this_thread == cudaStreamCaptureModeRelaxed:
+        never invalidate any capturing cuda streams whatsoever.
+    elif capture_mode_this_thread == cudaStreamCaptureModeThreadLocal:
+        invalidate any cuda streams for which cudaStreamBeginCapture() was called by this
+        thread, except for streams whose capture mode is cudaStreamCaptureModeRelaxed.
+    elif capture_mode_this_thread == cudaStreamCaptureModeGlobal:
+        invalidate all cuda streams that are currently capturing on any thread,
+        except for streams whose capture mode is cudaStreamCaptureModeRelaxed and for
+        streams for which cudaStreamCaptureBegin() was called with
+        cudaStreamCaptureModeThreadLocal on a thread other than this one.
+
+    In practice, changed the current capture mode to
+    cudaStreamCaptureModeRelaxed in particular is helpful for enabling
+    developers to do "unsafe" things that we know are safe in our
+    case.
+    """
     cudart = torch.cuda.cudart()
     old_mode = cudart.cudaThreadExchangeStreamCaptureMode(new_mode)
     try:
